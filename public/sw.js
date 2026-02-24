9812ac8db2d56369626f2b624ab67cb9a3581a04
u8902054545-design <u8902054545@gmail.com>
2026-02-24 03:50:49 +0300

    feat: navigation, slots and actions setup

---
+++ public/sw.js
@@ -0,0 +1,5 @@
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', () => clients.claim());
+self.addEventListener('fetch', (event) => {
+  event.respondWith(fetch(event.request));
+});
